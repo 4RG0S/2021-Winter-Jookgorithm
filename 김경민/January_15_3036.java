98351f93d67f54c2566f8c53ebe2f3564ecd56bc
mgpocky <kkm1447@naver.com>
2021-01-15 19:40:08 +0900

    [21.01.15]3036.java

---
+++ 김경민/January_15_3036.java
@@ -0,0 +1,25 @@
+package January;
+
+import java.util.Scanner;
+
+public class January_15_3036 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int arr[] = new int[N];
+		int num = scan.nextInt();
+		for(int i=1;i<N;i++) {
+			arr[i] = scan.nextInt();
+			int gcd = 1;
+			for(int j=2;j<Integer.max(num, arr[i]);j++) {
+				if(num % j == 0 && arr[i] % j == 0)
+					gcd = j;
+			}
+			System.out.println(num/gcd + "/" + arr[i]/gcd);
+		}
+		
+	}
+
+}
